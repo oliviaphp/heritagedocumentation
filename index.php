51d583275217be1a188b432c16ff3f7cb1634431
Ibad Kureshi <ibad.kureshi@durham.ac.uk>
2017-03-08 13:00:34 +0000

    Added the index.php and revised style.css
    Converted HTML to PHP - made the div creation and the image loading
    dynamic. Edited the css to make a uniform background and to help in the
    animation

---
+++ index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta charset="UTF-8"> 
+    <title>Heritage Documentation and Protection</title>
+    <link rel="stylesheet" href="style.css" type="text/css" />
+    <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Vollkorn">
+    
+    <style>
+        body {
+        font-family: 'Vollkorn', serif;
+        }
+    </style>
+</head>
+
+<body>
+
+    <!--- Homepage -->
+    <div id="home" class="content">
+          <div class="main">
+              <h2>Heritage Documentation and Protection</h2>
+
+          </div>
+
+        <div class="project">
+            <h3>About the Project</h3>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        </div>
+    </div> 
+     <!--- End Homepage -->
+    
+     <!--- Team -->
+    <div id="team" class="panel">
+	    <div class="content">
+		    <h2>About the Team</h2>
+		    
+		
+	    </div>
+    </div>
+    <!--- End Team -->
+
+	<!-- Artifacts -->
+	
+	<div id="artifacts" class="panel">
+		<div class="content">
+			<h2>Show all Artifacts</h2>
+			<div id="imagelist">
+				<!-- Create the image array -->
+				<?php
+
+				$link = mysql_connect('localhost','olivia','t9Hpp02?')
+					or die('Could not connect: '.mysql.error());
+				// echo 'Connected successfully';
+				mysql_select_db('du_arch') or die('Could not select database');
+
+				// Peforming SQL query
+				$query = 'SELECT * FROM artefact';
+				$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+				// Printing results in HTML
+				if($result){
+
+					while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+						//echo "\t<br />\n";
+						$pano = glob('../olivia/Sites/'.$line[Site]."/".$line[uuid]."/*.*");
+						echo '<a button onclick=myFunction("'.$line[uuid].'")><img src="' . $pano[0] .'" style="width:304px;height:228px" alt="'. $line[uuid] .'"/></a>';	
+						echo "\r\n";
+				 	}
+				}
+
+				// Free resultset
+				mysql_free_result($result);
+
+				// Closing Connection
+				mysql_close($link);
+				?>
+			</div><!--imagelist-->
+			
+		</div><!--content-->
+	</div><!--artifacts-->
+
+	<div class="section-container">
+<!-- Create the image related divs -->
+		<?php
+
+		$link = mysql_connect('localhost','olivia','t9Hpp02?')
+			or die('Could not connect: '.mysql.error());
+		// echo 'Connected successfully';
+		mysql_select_db('du_arch') or die('Could not select database');
+
+		// Peforming SQL query
+		$query = 'SELECT * FROM artefact';
+		$result = mysql_query($query) or die('Query failed: ' . mysql_error());
+
+		// Printing results in HTML
+		if($result){
+
+			while ($line = mysql_fetch_array($result, MYSQL_ASSOC)) {
+				//echo "\t<br />\n";
+				echo "<div id='".$line[uuid]."' class='page currentpage' style='z-index:499; left: 30%;'>\r\n";
+				echo "<a button onclick=closeFunction('".$line[uuid]."')>[<u>Close</u>]</a>\r\n";
+			    echo "<h3 class='title'>Artifact Details</h3>\r\n";
+				echo "<ul>
+					<li>Material: ".$line[Material]."</li>
+					<li>Condition: ".$line[Condit]."</li>
+					<li>Object Type: ".$line[ObjectType];
+				echo "</ul></div>\r\n";
+				//ObjectType,ObjectSubType,ObjectPart,X,Y,Z,Material,MaterialSbType,SurfaceTexture,Decoration,DecorationType,Condit,LootingRisk,VandalismRisk,RemovalRisk,DamageRisk,FreeText,Latitude,Longitude,hasParent,ParentID,UserID,uuid,Site
+		 	}
+
+		}
+
+		// Free resultset
+		mysql_free_result($result);
+
+		// Closing Connection
+		mysql_close($link);
+
+		?>
+	</div><!-- section container-->
+
+<!-- JavaScript -->
+<script>
+	function myFunction(divID) {
+	    var x = document.getElementById(divID);
+	    var elements = document.getElementsByClassName("page currentpage"),
+		n = elements.length;
+		for (var i = 0; i < n; i++) {
+			var e = elements[i];
+			e.style.display = 'none';
+		}
+	    if (x.style.display === 'none') {
+		x.style.display = 'block';
+	    } else {
+		x.style.display = 'none';
+	    }
+	}
+	function closeFunction(divID) {
+	    var x = document.getElementById(divID);
+		x.style.display = 'none';
+	}
+</script>
+
+	<!-- End Artifacts -->
+	
+    <!-- Header with Navigation -->
+	<div id="header">
+		<h1>Page Transitions with CSS3</h1>
+		<ul id="navigation">
+			<li><a id="link-home" href="#home">Home</a></li>
+			<li><a id="link-team" href="#team">About the Team</a></li>
+			<li><a id="link-artifacts" href="#artifacts">Show all Artifacts</a></li>
+		</ul>
+	</div>
+</body>
+
+</html>
